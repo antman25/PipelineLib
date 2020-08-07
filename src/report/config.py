d214540976568af8691f631c32a50dffea68e21f
Anthony Magner <anthony.magner@gmail.com>
2020-08-06 23:57:36 -0600

    asdf

---
+++ src/report/config.py
@@ -0,0 +1,11 @@
+import logging
+
+#https://docs.python.org/3/howto/logging.html
+#https://docs.python.org/3/library/logging.html#formatter-objects
+LOG_LEVEL = logging.DEBUG
+# LOG_FORMATTER
+TEXT_COLOR_THEME = 'light'
+LOG_FILE = 'output.log'
+
+SOME_PORT = 8080
+SOME_NAME = 'anthony'
--- src/report/test2.py
+++ src/report/test2.py
@@ -1,6 +1,72 @@
 #!/usr/bin/python3
-
+from os import path, makedirs
+import importlib
+import logging
 import json
+import sys
+
+log = logging.getLogger('test')
+
+COLORS = {'DEBUG': 'cyan', 'INFO': 'green', 'WARNING': 'yellow', 'ERROR': 'red', 'CRITICAL': 'red', }
+
+NO_COLORS = {'DEBUG': '', 'INFO': '', 'WARNING': '', 'ERROR': '', 'CRITICAL': '', }
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+
+stream = sys.stdout
+isatty = stream.isatty()
+console_hdlr = logging.StreamHandler(stream)
+
+def get_log_colors(theme_color=None):
+    """Return a tuple containing the log format string and a log color dict"""
+    if theme_color == 'light':
+        text_color_theme = 'white'
+    elif theme_color == 'dark':
+        text_color_theme = 'black'
+    else:  # Anything else produces nocolor
+        return '%(name)-25.25s%(reset)s %(message)s%(reset)s', NO_COLORS
+
+    return f'%(name)-25.25s%(reset)s %({text_color_theme})s%(message)s%(reset)s', COLORS
+
+def format_logs(formatter=None, theme_color=None):
+    """
+    You may either use the formatter parameter to provide your own
+    custom formatter, or the theme_color parameter to use the
+    built in color scheme formatter.
+    """
+    if formatter:
+        console_hdlr.setFormatter(formatter)
+    # if isatty and not True:
+    elif isatty:
+        from colorlog import ColoredFormatter  # noqa
+        log_format, colors_dict = get_log_colors(theme_color)
+        color_formatter = ColoredFormatter(
+            "%(asctime)s %(log_color)s%(levelname)-8s%(reset)s " + log_format,
+            datefmt="%H:%M:%S",
+            reset=True,
+            log_colors=colors_dict,
+        )
+        console_hdlr.setFormatter(color_formatter)
+    else:
+        console_hdlr.setFormatter(logging.Formatter("%(asctime)s %(levelname)-8s %(name)-25s %(message)s"))
+    root_logger.addHandler(console_hdlr)
+
+def get_config(config_path):
+    log.info ('Loading: %s' % config_path)
+    config_fullpath = config_path
+    if not path.exists(config_fullpath):
+        log.error('I cannot find the config file %s.' % config_fullpath)
+        exit(-1)
+
+    try:
+        config = __import__(path.splitext(path.basename(config_fullpath))[0])
+        log.info('Config check passed...')
+        return config
+    except Exception:
+        log.exception('I could not import your config from %s, please check the error below...' % config_fullpath)
+        exit(-1)
+
 
 class Config():
     def getConfig(self):
@@ -12,8 +78,8 @@ class MyConfig(Config):
 
         a = MyConn('192.168.0.1',5000)
         b = MyVars()
-        print (a)
-        print (b)
+        log.info (a)
+        log.info (b)
         d = a.getConfig()
         v = b.getConfig()
 
@@ -51,9 +117,22 @@ class MyVars(Config):
     def getConfig(self):
         return [self.var1] + self.var2
 
+if __name__ == '__main__':
+    config = get_config('config.py')
 
-c = MyConfig('ant')
+    if hasattr(config, 'LOG_FORMATTER'):
+        format_logs(formatter=config.BOT_LOG_FORMATTER)
+    else:
+        format_logs(theme_color=config.TEXT_COLOR_THEME)
 
-j= json.dumps(c.getConfig(), sort_keys=True,indent=4)
-print(j)
-#'{"a": 1, "b": 2}'
+    if config.LOG_FILE:
+        hdlr = logging.FileHandler(config.LOG_FILE)
+        hdlr.setFormatter(logging.Formatter("%(asctime)s %(levelname)-8s %(name)-25s %(message)s"))
+        log.addHandler(hdlr)
+
+    log.setLevel(config.LOG_LEVEL)
+    log.info(config)
+    i = MyConfig(config.SOME_NAME)
+    j = json.dumps(i.getConfig(), sort_keys=True,indent=4)
+    log.info(j)
+
